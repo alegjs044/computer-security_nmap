b37b24859a774c78c1b6f23928389d2ae1e377a8
alegjs044 <alegjs044@gmail.com>
2024-04-20 20:44:11 -0600

    Update nmap.py

--- nmap.py
+++ nmap.py
@@ -1,8 +1,8 @@
 import nmap
 
-def run_nmap_scan(target, ports, arguments, use_sudo):
+def run_nmap_scan(hosts, ports, arguments, use_sudo):
       scanner = nmap.PortScanner()
-      scan_result = scanner.scan(hosts=target, ports=ports, arguments=arguments, sudo=use_sudo)
+      scan_result = scanner.scan(hosts=hosts, ports=ports, arguments=arguments, sudo=use_sudo)
 
       print("\n---------------- Scan Results ----------------")
       for host, host_info in scan_result['scan'].items():
@@ -21,7 +21,7 @@ def main():
       target = input("Host/IP: ").strip()
       ports = input("Port(s): ").strip()
       arguments = input("map-arguments (-sn, -SP): ").strip()
-      use_sudo = input("Run the command as super user? (y/n): ").strip().upper() == "Y", "y", "yes"
+      use_sudo = input("¿Ejecutar como super usuario?(y/n): ").strip().upper() == "Y", "y", "yes"
 
       try:
           port_list = [p.strip() for p in ports.split(',')]  
